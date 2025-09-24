bf86de0690ac29ae5014f5460b7791dcfb9a61b8
Efl <133080730+Extrafly1@users.noreply.github.com>
2025-09-25 02:01:24 +0300

    add palette

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.media:media:1.6.0")
 // Compose
+    implementation("androidx.palette:palette-ktx:1.0.0")
     implementation("androidx.compose.ui:ui:1.5.0")
     implementation("androidx.compose.material3:material3:1.2.0")
     implementation("androidx.compose.ui:ui-tooling-preview:1.5.0")
